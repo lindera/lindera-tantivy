e061ef5613f3fbd96abac72a218559e681496d8b
Minoru Osuka <minoru.osuka@gmail.com>
2023-01-08 21:40:58 +0900

    Fix build errors

--- examples/ko-dic_example.rs
+++ examples/ko-dic_example.rs
@@ -65,12 +65,13 @@ fn main() -> tantivy::Result<()> {
         dictionary,
         user_dictionary: None,
         mode: Mode::Normal,
+        with_details: false,
     };
 
     // register Lindera tokenizer
     index
         .tokenizers()
-        .register("lang_ko", LinderaTokenizer::with_config(config).unwrap());
+        .register("lang_ko", LinderaTokenizer::from_config(config).unwrap());
 
     // create index writer
     let mut index_writer = index.writer(50_000_000)?;
